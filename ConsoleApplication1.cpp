7658c1d1c01247d19c3cb0237690aab82eba9c9f
shvkvchi <db2007surgut@gmail.com>
2025-10-15 01:47:41 -0500

    Add project files.

---
+++ ConsoleApplication1.cpp
@@ -0,0 +1,355 @@
+// ConsoleApplication1.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+
+#include <iostream>
+#include <string>
+#include <vector>
+#include <map>
+#include <random>
+#include <algorithm>
+
+
+class Coordinates {      
+public:            
+    std::vector<int> open;
+    std::vector<int> closed;
+};
+
+std::string generateRandomString(int length)
+{
+    // Define the list of possible characters
+    const std::string CHARACTERS
+        = "ABCDEFGHIJKLMNOPQRSTUVWXYZabcdefghijklmnopqrstuv"
+        "wxyz0123456789";
+
+    // Create a random number generator
+    std::random_device rd;
+    std::mt19937 generator(rd());
+
+    // Create a distribution to uniformly select from all
+    // characters
+    std::uniform_int_distribution<> distribution(
+        0, CHARACTERS.size() - 1);
+
+    // Generate the random string
+    std::string random_string;
+    for (int i = 0; i < length; ++i) {
+        random_string
+            += CHARACTERS[distribution(generator)];
+    }
+
+    return random_string;
+}
+
+
+
+int main()
+{
+    // function
+    std::string yFunction = "((3+(5-2*(3+1))))";
+    // coordinates of parentheses
+    Coordinates insideOfParentheses;
+    // to check for copies
+    std::vector<int> openParenthesesIndexes;
+    std::vector<int> closedParenthesesIndexes;
+    // to choose last parentheses
+    int lastOpen;
+    // amount of parentheses
+    int parenthesesNum = 0;
+    // parts of function
+    std::vector<std::string> partsOfFunction;
+    // part of function
+    std::string prt;
+    // used parts
+    std::vector<int> alreadyPart = { 99999999 };
+    // map for parts
+    std::map<std::string, int> keys;
+    // key var
+    std::string k;
+    // flag
+    bool flag = false;
+    // length of key
+    int keyLength = 10;
+    double result = 0;
+    char lastSign = '\0';
+    std::string strNum;
+    double num = 0;
+    bool neg = false;
+
+    int pos1;
+
+    int j;
+    //std::getline(std::cin, yFunction);
+    yFunction.erase(remove(yFunction.begin(), yFunction.end(), ' '), yFunction.end());
+    std::cout << "Function >>> " << yFunction << std::endl;
+
+    /*for (int i = yFunction.length() - 2; i >= 1; i--)
+    {
+        if (yFunction[i] == '*' && yFunction[i - 1] == ')' && yFunction[i + 1] == '(')
+        {
+            yFunction.erase(i, 1);
+        }
+    }
+    std::cout << "Function after removing * >>> " << yFunction << std::endl;*/
+
+    for (int i = yFunction.length() - 1; i >= 0; i--)
+    {
+        if (yFunction[i] == '*' || yFunction[i] == '/')
+        {
+            if (isdigit(yFunction[i + 1]) && isdigit(yFunction[i - 1]))
+            {
+                j = i + 1;
+                while (isdigit(yFunction[j]))
+                {
+                    j++;
+                }
+                yFunction.insert(j, 1, ')');
+                j = i - 1;
+                while (isdigit(yFunction[j]))
+                {
+                    j--;
+                }
+                yFunction.insert(j + 1, 1, '(');
+            }
+            else if (yFunction[i + 1] == '(' && yFunction[i - 1] == ')')
+            {
+                j = i + 1;
+                while (yFunction[j] != ')')
+                {
+                    j++;
+                }
+                yFunction.insert(j, 1, ')');
+                j = i - 1;
+                while (yFunction[j] != '(')
+                {
+                    j--;
+                }
+                yFunction.insert(j + 1, 1, '(');
+            }
+        }
+    }
+    std::cout << "Function after parentheses for * and / >>> " << yFunction << std::endl;
+
+    for (int i = yFunction.length() - 1; i >= 0; i--)
+    {
+        if (yFunction[i] != '*' && yFunction[i] != '/' && yFunction[i] != '+')
+        {
+            if (i < yFunction.length() - 1 && yFunction[i] != ')' && yFunction[i] != '(' && yFunction[i + 1] == '(')
+            {
+                pos1 = i + 1;
+                while (isalpha(yFunction[i]) || yFunction[i] == '-' || yFunction[i] == 'x')
+                {
+                    i--;
+                }
+                yFunction.insert(i + 1, 1, ')');
+                yFunction.insert(i, 1, '(');
+            }
+            if (i > 0 && yFunction[i - 1] == ')' && yFunction[i] != '(' && yFunction[i] != ')')
+            {
+                pos1 = i;
+                if (yFunction[i] == '-')
+                {
+                    i++;
+                }
+                while (isalpha(yFunction[i]) || yFunction[i] == 'x')
+                {
+                    i++;
+                }
+                yFunction.insert(i + 1, 1, ')');
+                yFunction.insert(pos1, 1, '(');
+            }
+        }
+    }
+    std::cout << "Function after parentheses for * >>> " << yFunction << std::endl;
+
+    if (yFunction.find("(") != std::string::npos && yFunction.find(")") != std::string::npos) // Parentheses
+    {
+        for (int i = 0; i < yFunction.length(); i++)
+        {
+            if (yFunction[i] == '(')
+            {
+                parenthesesNum++;
+            }
+        }
+        std::cout << "Number of Parentheses >>> " << parenthesesNum << std::endl;
+
+        while (parenthesesNum != openParenthesesIndexes.size()) // open and closed parantheses index
+        {
+            for (int i = 0; i < yFunction.length(); i++)
+            {
+                if (yFunction[i] == '(' && std::find(openParenthesesIndexes.begin(), openParenthesesIndexes.end(), i) == openParenthesesIndexes.end())
+                {
+                    lastOpen = i;
+                }
+            }
+            openParenthesesIndexes.push_back(lastOpen);
+            insideOfParentheses.open.push_back(lastOpen);
+            for (int i = lastOpen; i < yFunction.length(); i++)
+            {
+                if (yFunction[i] == ')' && std::find(closedParenthesesIndexes.begin(), closedParenthesesIndexes.end(), i) == closedParenthesesIndexes.end())
+                {
+                    insideOfParentheses.closed.push_back(i);
+                    closedParenthesesIndexes.push_back(i);
+                    break;
+                }
+            }
+        }
+
+        for (int i = 0; i < insideOfParentheses.open.size(); i++) 
+        {
+            prt = "";
+            for (int j = i - 1; j >= 0; j--) // iterate thru already included parts
+            {
+                if (insideOfParentheses.open[j] > insideOfParentheses.open[i] && insideOfParentheses.closed[j] < insideOfParentheses.closed[i] && std::find(alreadyPart.begin(), alreadyPart.end(), j) == alreadyPart.end() && j != alreadyPart.back()) // if parenthesis inside and wasnt already inclued 
+                {
+                    if (yFunction.substr(insideOfParentheses.open[j] - insideOfParentheses.open[i], 1) == "/" || yFunction.substr(insideOfParentheses.open[j] - insideOfParentheses.open[i], 1) == "-" || yFunction.substr(insideOfParentheses.open[j] - insideOfParentheses.open[i], 1) == "+")
+                    {
+                        prt = prt + yFunction.substr(insideOfParentheses.open[j] - insideOfParentheses.open[i], 1);
+                    }
+                    if (yFunction.substr(insideOfParentheses.open[i] + 1, insideOfParentheses.open[j] - insideOfParentheses.open[i] - 1) != "" && yFunction.substr(insideOfParentheses.open[i] + 1, 1) != "(") 
+                    {
+                        flag = false;
+                        for (auto const& [key, val] : keys)
+                        {
+                            if (val == j) {
+                                prt = prt + yFunction.substr(insideOfParentheses.open[i], insideOfParentheses.open[j] - insideOfParentheses.open[i]) + "(" + key + "))";
+                                flag = true;
+                                break;
+                            }
+                        }
+                        if (!flag)
+                        {
+                            k = generateRandomString(keyLength);
+                            keys[k] = j;
+                            prt = prt + yFunction.substr(insideOfParentheses.open[i], insideOfParentheses.open[j] - insideOfParentheses.open[i]) + "(" + k + "))";
+                        }
+                    }
+                    else
+                    {
+                        for (auto const& [key, val] : keys)
+                        {
+                            if (val == j) {
+                                prt = prt + "(" + key + ")";
+                                flag = true;
+                                break;
+                            }
+                        }
+                        if (!flag)
+                        {
+                            k = generateRandomString(keyLength);
+                            keys[k] = j;
+                            prt = prt + "(" + k + ")";
+                        }
+                    }
+                    alreadyPart.push_back(j);
+                }
+            }
+            if (prt != "")
+            {
+                partsOfFunction.push_back(prt);
+            }
+            else
+            {
+                partsOfFunction.push_back(yFunction.substr(insideOfParentheses.open[i], insideOfParentheses.closed[i] - insideOfParentheses.open[i] + 1));
+            }
+        }
+        for (int i = 0; i < partsOfFunction.size(); i++)
+        {
+            std::cout << "Part " << i << " >>> " << partsOfFunction[i] << std::endl;
+        }
+        for (auto const& [key, val] : keys)
+        {
+            std::cout << "Key " << " >>> " << key << " Part >>> " << partsOfFunction[keys[key]] << std::endl;
+        }
+    }
+
+    for (int part = 0; part < partsOfFunction.size(); part++) // get rid of parentheses and solve
+    {
+        flag = false;
+        k = "";
+        for (int i = partsOfFunction[part].size(); i >= 0; i--)
+        {
+            if (partsOfFunction[part][i] == '(' && i != 0 && partsOfFunction[part][i - 1] == ')')
+            {
+                partsOfFunction[part][i] = '*';
+            }
+            else if (partsOfFunction[part][i] == '(' || partsOfFunction[part][i] == ')' )
+            {
+                partsOfFunction[part].erase(i, 1);
+            }
+        }
+        for (auto const& [key, val] : keys)
+        {
+            if (partsOfFunction[part].find(key) != std::string::npos)
+            {
+                partsOfFunction[part].replace(partsOfFunction[part].find(key), key.length(), partsOfFunction[keys[key]]);
+            }
+        }
+        std::cout << "Part " << part << " >>> " << partsOfFunction[part] << std::endl;
+        for (int i = 0; i < partsOfFunction[part].size(); i++)
+        {
+            strNum = "";
+            num = 0;
+            if (partsOfFunction[part][i] == '*' || partsOfFunction[part][i] == '/' || partsOfFunction[part][i] == '-' || partsOfFunction[part][i] == '+')
+            {
+                if (partsOfFunction[part][i] == '-' && i > 0 && (partsOfFunction[part][i - 1] == '*' || partsOfFunction[part][i - 1] == '/' || partsOfFunction[part][i - 1] == '-' || partsOfFunction[part][i - 1] == '+'))
+                {
+                    neg = true;
+                }
+                else
+                {
+                    lastSign = partsOfFunction[part][i];
+                }
+            }
+            if (isdigit(partsOfFunction[part][i]))
+            {
+                strNum += partsOfFunction[part][i];
+                while (i < partsOfFunction[part].size() && (isdigit(partsOfFunction[part][i + 1]) || partsOfFunction[part][i + 1] == '.'))
+                {
+                    i++;
+                    strNum += partsOfFunction[part][i];
+                }
+                num = std::stod(strNum);
+                if (lastSign != '\0' && strNum != "")
+                {
+                    if (neg)
+                    {
+                        num = -num;
+                        neg = false;
+                    }
+                    switch (lastSign)
+                    {
+                    case '*':
+                        result *= num;
+                        lastSign = '\0';
+                        break;
+                    case '/':
+                        result /= num;
+                        lastSign = '\0';
+                        break;
+                    case '-':
+                        result -= num;
+                        lastSign = '\0';
+                        break;
+                    case '+':
+                        result += num;
+                        lastSign = '\0';
+                        break;
+                    default:
+                        break;
+                    }
+                }
+                else if (strNum != "")
+                {
+                    result = num;
+                }
+            }
+        }
+        partsOfFunction[part] = std::to_string(result);
+        std::cout << "Result " << part << " >>> " << partsOfFunction[part] << std::endl;
+        result = 0;
+    }
+
+
+    return 0;
+}
